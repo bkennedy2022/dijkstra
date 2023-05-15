7de1b4fc29f1e89f66fc3fb574ed1a6d392ab8b8
Brooks Kennedy <brooksekennedy@gmail.com>
2023-05-15 14:55:14 -0700

    implemented dijkstra algorithm logic on backend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from algorithm import Algorithm
 
 app = Flask(__name__)
 CORS(app) # enable CORS for all origins (for now...)
@@ -15,7 +16,19 @@ def graph():
     print("loading sample graph data...")
     with open("backend/graphSample.txt", "r") as f:
         # print(f.read())
-        return f.read()
+        # matrix = f.read()
+
+        matrix = []
+        for row in f:
+            matrix.append([int(x) for x in row.split()])
+
+        print(matrix)
+
+        g = Algorithm(len(matrix))
+        g.graph = matrix
+        g.dijkstra(0)
+
+        return matrix
 
 # serves project locally without needing to set flask environment variables
 if __name__ == "__main__":
